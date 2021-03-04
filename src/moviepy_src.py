b7b16e321692d48f29627bfdba2f4e19a5c3df61
Oscar Barragán <oscaribv@gmail.com>
2021-03-04 11:11:03 +0000

    Now tango works with moviepy

--- src/moviepy_src.py
+++ src/moviepy_src.py
@@ -7,7 +7,7 @@ gif_name = system
 fps = frate
 file_list = sorted(glob.glob(system+'/*.png')) # Get all the pngs in the current directory
 clip = mpy.ImageSequenceClip(file_list, fps=1./fps)
-clip = clip.resize(height=360)
+#clip = clip.resize(height=360)
 clip.write_gif(system+'/'+system+'.gif')
 clip.write_videofile(system+'/'+system+'.mp4')
 
